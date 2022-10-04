66c5d17f6ae05024b3f2edeca991b2d4e562f84f
Le Minh Nhat Dang <lmndang.toronto@gmail.com>
2022-10-03 22:19:40 -0400

    Designing an E-Commerce Schema

--- server.js
+++ server.js
@@ -6,15 +6,64 @@ const { graphqlHTTP } = require("express-graphql");
 
 const schema = buildSchema(`
     type Query {
-        description: String
-        price: Float
+        products: [Product]
+        orders: [Order]
+    }
 
+    type Product{
+        id: ID!
+        description: String!
+        reviews: [Review]
+        price: Float!
+    }
+
+    type Review {
+        rating: Int
+        comment: String
+    }
+
+    type Order {
+        date: String!
+        subtotal: Float!
+        items: [OrderItem]
+    }
+
+    type OrderItem {
+        product: Product!
+        quantity: Int!
     }
 `);
 
 const root = {
-  description: "Red Shoe",
-  price: 42.99,
+  products: [
+    {
+      id: "1",
+      description: "Red Shoe",
+      price: 42.99,
+    },
+    {
+      id: "2",
+      description: "Blue Jean",
+      price: 12.99,
+    },
+  ],
+
+  orders: [
+    {
+      date: "2005-05-05",
+      subtotal: 91.98,
+      items: [
+        {
+          product: {
+            id: "1",
+            description: "Old Red Shoe",
+            price: 45.99,
+          },
+          quantity: 2,
+        },
+      ],
+    },
+  ],
 };
 
 const app = express();
