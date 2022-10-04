e4e56d8f9c9f91a6868e1b2e51e65e26d805ee1e
Le Minh Nhat Dang <lmndang.toronto@gmail.com>
2022-10-03 21:58:37 -0400

    Add basic graphQL to the server

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const { buildSchema } = require("graphql");
+
+const { graphqlHTTP } = require("express-graphql");
+
+const schema = buildSchema(`
+    type Query {
+        description: String
+        price: Float
+
+    }
+`);
+
+const root = {
+  description: "Red Shoe",
+  price: 42.99,
+};
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+  })
+);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
