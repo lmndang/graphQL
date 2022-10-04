1d3b9d2261604fb264ae14e52bf1e608b71552db
Le Minh Nhat Dang <lmndang.toronto@gmail.com>
2022-10-03 23:07:37 -0400

    Add resolvers

--- server.js
+++ server.js
@@ -12,6 +12,20 @@ const typesArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
 
 const schema = makeExecutableSchema({
   typeDefs: typesArray,
+  resolvers: {
+    Query: {
+      products: async (parent) => {
+        console.log("Getting the products...");
+        const product = await Promise.resolve(parent.products);
+        return product;
+      },
+      orders: async (parent) => {
+        console.log("Getting the orders...");
+        const order = await Promise.resolve(parent.orders);
+        return order;
+      },
+    },
+  },
 });
 
 const root = {
